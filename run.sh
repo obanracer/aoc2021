ce7d0cc92dce95fb229bc9e5375046bbb20f069f
ObanRacer <desimone.brunof@gmail.com>
2021-12-21 03:41:10 -0300

    minor run.sh print change

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 echo "running day $1, puzzle $2..."
+echo "========================================================="
 cd $1
 cd $2
 gcc solution.c -o solution
 ./solution
+echo "========================================================="
 echo "done."
